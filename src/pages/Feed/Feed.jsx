3c95f41c77da9ed362b86374d6bf9d57e79e2ee0
Brayan Espinoza <bryan.espinoza97@hotmail.com>
2022-08-11 22:00:41 -0600

    create main page and top bar

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -1,10 +1,14 @@
+import { TopMenu } from '../../components/TopMenu/TopMenu'
 import { MainPage } from '../Main/MainPage'
 import styles from './Feed.module.css'
 
 export const Feed = () => {
     return (
         <div className={styles.container}>
-            
+            <TopMenu/>
+            <MainPage/>
+            <MainPage/>
+            <MainPage/>
             <MainPage/>
         </div>
     )
--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -1,12 +1,18 @@
+import { RecamaraWB } from '../../assets/gallery'
 import { CarouselComponent } from '../../components/Carousel/CarouselComponent'
 import styles from './MainPage.module.css'
 
+
 export const MainPage = () => {
     return (
-        <div className={styles.container}>
+        <section className={styles.container}>
             {/* <CarouselComponent/> */}
-            <h1>Muebles Rusticos El Chaparral</h1>
+            <div className={styles.title_container}>
+                <h1>Muebles Rusticos El Chaparral</h1>
+                <button onClick={() => alert('Muebles Rusticos El chaparral')} className={styles.button_contact}>Contactanos</button>
+            </div>
+            <img src={RecamaraWB} alt='Recamara' title='Recamara' />
 
-        </div>
+        </section>
     )
 }
